98ead7e05582e593933c3910f3bdd67d81c1d635
xiaolidefeidao <610777661@qq.com>
2018-10-06 23:28:57 +0800

    优化冒泡排序1
    添加isChange标记，记录一趟冒泡中是否发生元素互换

--- 冒泡排序.js
+++ 冒泡排序.js
@@ -5,17 +5,21 @@ function bubbleSort(arr) {
         throw Error('type error')
         return
     }
-    let temp,
+    let temp, isChange,
         len = arr.length;
     for (let i = 0; i < len - 1; i++) {
+        //添加isChange标记，记录一趟冒泡中是否发生元素互换
+        isChange = false
         for (let j = 0; j < len - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 // [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
                 temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
+                isChange = true;
             }
         }
+        if(!isChange) break;
     }
     return arr
 }
