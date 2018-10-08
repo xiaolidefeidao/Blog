4f210791edd3719e886f63d3a7b7d2e61b883758
xiaolidefeidao <610777661@qq.com>
2018-10-08 23:50:03 +0800

    优化快速排序
    在原数组上进行元素交换，节省空间

--- quickSort.js
+++ quickSort.js
@@ -1,40 +1,34 @@
 'use strict';
 
-function quickSort(arr) {
-    if (arr.length <= 1) return arr;
+function quickSort(arr, start, end) {
+    if (end - start < 1) return;
     // let [anchor, i, j] = [arr[0], 0, arr.length-1];
     let cache;
-    let anchor = arr[0];
-    let i = 0;
-    let j = arr.length - 1;
+    //anchor作为基准值
+    let anchor = arr[start];
+    let i = start;
+    let j = end;
     while (i < j) {
-        while (j > i) {
-            if (arr[j] < anchor) {
-                break;
-            } else {
-                j--
-            }
+        while (j > i && arr[j] > anchor) {
+            j--
         }
-        while (i < j) {
-            if (arr[i] <= anchor) {
-                i++
-            } else {
-                break;
-            }
+        while (i < j && arr[i] <= anchor) {
+            i++
         }
         // [arr[i], arr[j]] = [arr[j], arr[i]]
         cache = arr[i];
         arr[i] = arr[j];
         arr[j] = cache;
     }
-    // [arr[0], arr[j]] = [arr[j], arr[0]];
-    arr[0] = arr[i];
-    arr[i] = anchor;
-    console.log(i)
-    console.log(j)
-    return quickSort(arr.slice(0, j)).concat([anchor], j < arr.length - 1 ? quickSort(arr.slice(j + 1)) : [])
 
+
+    // [arr[0], arr[j]] = [arr[j], arr[0]];
+    arr[start] = arr[i];
+    arr[i] = anchor;
+    quickSort(arr, start, i - 1);
+    quickSort(arr, i + 1, end)
 }
 
 let a = [23, 56, 2, 31, 1, 6, 8, 7, 90, 56];
-console.log(quickSort(a))
+quickSort(a, 0, a.length - 1)
+console.log(a)
