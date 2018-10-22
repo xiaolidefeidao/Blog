273ca436b6cbf926be86d6b0fd7e57d110271aa4
Thomasflyi <xiaolidefeidao@users.noreply.github.com>
2018-10-22 19:57:41 +0800

    Create 回文（二）.js

---
+++ 回文（二）.js
@@ -0,0 +1,20 @@
+/**
+ *Created by 01369606 on 2018/10/22.
+ */
+
+'use strict'
+
+//判断一个整数是否为‘回文’，例如 1221是‘回文’，1232不是
+function Solution(num) {
+  //默认num为整数
+  if (num === 0) return true;
+  if (num < 0) return false;
+  let ans = 0, temp = num;
+  while (temp != 0) {
+    ans = ans * 10 + temp % 10;
+    temp = parseInt(temp / 10);
+  }
+  return ans === num;
+}
+
+console.log(Solution(112354453211));
