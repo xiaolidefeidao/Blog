820c7450343a34438fde7219807fd23920e39a54
xiaolidefeidao <610777661@qq.com>
2018-11-17 22:30:00 +0800

    fix listeners

--- EventEmitter.js
+++ EventEmitter.js
@@ -3,87 +3,87 @@
 //在事件的订阅、发布、取消过程中都可以传入多个回调函数
 
 class EventEmitter {
-  constructor(num = 10) {
-    this.maxListeners = num;
-    this.$events = new Map();
-  }
-
-  getMaxListeners() {
-    return this.maxListeners;
-  }
-
-  setMaxListeners(num) {
-    this.maxListeners = num;
-  }
-
-  $on(event, ...cbs) {
-    if (this.$events.has(event)) {
-      let listeners = this.$events.get(event).concat(cbs);
-      if (listeners.length > this.getMaxListeners()) {
-        console.log('订阅失败，超出最大监听器数！')
-      } else {
-        this.$events.set(event, listeners)
-      }
-    } else {
-      if (cbs.length > this.getMaxListeners()) {
-        console.log('订阅失败，超出最大监听器数！')
-      } else {
-        this.$events.set(event, cbs)
-      }
+    constructor(num = 10) {
+        this.maxListeners = num;
+        this.$events = new Map();
     }
-  }
 
-  $once(event, ...cbs) {
-    let wraps=[];
-    for (let cb of cbs) {
-      function wrap() {
-        cb(...arguments);
-        this.$off(event, wrap)
-      }
-
-      wrap.cb = cb;
-      wraps.push(wrap);
+    getMaxListeners() {
+        return this.maxListeners;
     }
-    this.$on(event, ...wraps)
-  }
 
-  $off(event, ...cbs) {
-    if (this.$events.has(event) === false) return;
-    if (cbs.length === 0) {
-      this.$events.delete(event);
-      return;
+    setMaxListeners(num = 10) {
+        this.maxListeners = num;
     }
-    let listeners = this.$events.get(event);
-    for (const cb of cbs) {
-      listeners = listeners.filter(item => {
-        return cb !== item && cb !== item.cb;
-      })
-    }
-    listeners.length === 0 ? this.$events.delete(event) : this.$events.set(event, listeners)
-  }
 
-  $emit(event, ...args) {
-    if (this.$events.has(event) === false) return;
-    let listeners = this.$events.get(event);
-    listeners.map(cb => {
-      cb.call(this, ...args)
-    })
-  }
+    $on(event, ...cbs) {
+        if (this.$events.has(event)) {
+            this.$events.set(event, this.$events.get(event).concat(cbs))
+        } else {
+            if (this.$events.size === this.getMaxListeners()) {
+                console.log('订阅失败，超出最大监听器数！')
+            } else {
+                this.$events.set(event, cbs)
+            }
+        }
+    }
+
+    $once(event, ...cbs) {
+        let wraps = [];
+        for (let cb of cbs) {
+            function wrap() {
+                cb(...arguments);
+                this.$off(event, wrap)
+            }
+
+            wrap.cb = cb;
+            wraps.push(wrap);
+        }
+        this.$on(event, ...wraps)
+    }
+
+    $off(event, ...cbs) {
+        if (this.$events.has(event) === false) return;
+        if (cbs.length === 0) {
+            this.$events.delete(event);
+            return;
+        }
+        let fns = this.$events.get(event);
+        for (const cb of cbs) {
+            fns = fns.filter(item => {
+                return cb !== item && cb !== item.cb;
+            })
+        }
+        fns.length === 0 ? this.$events.delete(event) : this.$events.set(event, fns)
+    }
+
+    $emit(event, ...args) {
+        if (this.$events.has(event) === false) return;
+        let fns = this.$events.get(event);
+        fns.map(cb => {
+            cb.call(this, ...args)
+        })
+    }
 }
 
 let e1 = new EventEmitter(100);
 
 function en1() {
-  "use strict";
-  console.log('en1------------------', ...arguments);
+    "use strict";
+    try {
+        console.log(this.maxListeners)
+    } catch (e) {
+        console.log(e)
+    }
+    console.log('en1------------------', ...arguments);
 }
 
 function en2() {
-  "use strict";
-  console.log('en2------------------', ...arguments);
+    "use strict";
+    console.log('en2------------------', ...arguments);
 }
 
-e1.$on('x', en1, () => {
+/*e1.$on('x', en1, () => {
   "use strict";
   console.log('除非 off x，否则我不会消失')
 });
@@ -97,11 +97,15 @@ e1.$once('x', () => {
 });
 e1.$emit('x');
 e1.$emit('x', 456);
-e1.setMaxListeners(2);
 e1.$on('x', en2);
 e1.$emit('x');
 e1.$off('x', en1, en2);
 e1.$emit('x');
 e1.$off('x');
 e1.$emit('x');
-console.log(e1.$events.has('x'));
+console.log(e1.$events.has('x'));*/
+e1.$on('x', en1)
+e1.setMaxListeners(1)
+e1.$once('y', en1)
+e1.$emit('x')
+e1.$emit('y')
--- arrayOfSum（1）.js
+++ arrayOfSum（1）.js
@@ -1,18 +1,19 @@
 //给定一个整数数组和一个指定整数N，输出数组中两数之和等于N的元素下标(假定答案只有一组)
 //避免使用枚举法，以下方法时间复杂度为O(n)
 function findSumElement(arr, N) {
-  let obj = {}, len = arr.length;
-  for (let i = 0; i < len; i++) {
-    obj[arr[i]] = i;
-  }
-  for (let j = len - 1; j >= 0; j--) {
-    if(obj.hasOwnProperty(N-arr[j])){
-      return [j,obj[N-arr[j]]]
+    let obj = {}, len = arr.length;
+    for (let i = 0; i < len; i++) {
+        obj[arr[i]] = i;
     }
-  }
+    for (let j = len - 1; j >= 0; j--) {
+        if (obj.hasOwnProperty(N - arr[j])) {
+            return [j, obj[N - arr[j]]]
+        }
+    }
 }
-let arr=[1,2,3],N=3;
-console.log(findSumElement(arr,N));
+
+let arr = [1, 2, 3], N = 3;
+console.log(findSumElement(arr, N));
 
 //断舍离
 //心理韧性
